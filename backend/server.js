356ff917b79a94c72fd919c19d3a22e136e61539
tilecart <tilescartindia@gmail.com>
2020-04-04 11:56:19 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 3000
+const User = require('./models/user')
+const app = express()
+
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(console.log(`Connected to Database`))
+.catch(error => console.log("Error",error.message))
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/',async (req, res, next) => {
+    const user =  await User.find({  })
+    return res.status(200).json({ success: true, message: user });
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ dockerfile
@@ -0,0 +1,15 @@
+FROM node:latest
+
+WORKDIR /usr/app
+
+COPY package.json .
+COPY . .
+
+RUN npm install
+
+RUN npm install -g nodemon
+
+EXPOSE 3000
+
+CMD ["node", "src", "server.js"]
+
